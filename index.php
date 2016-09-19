bac4a35af662f54080f35dcf3f0e5f65a7e656ac
Kristian Munter Simonsen <Mokkatroffen@gmail.com>
2016-09-19 17:49:18 +0200

    Ny version med dårlige bakgrunnsbilder osv...

--- index.php
+++ index.php
@@ -1,59 +1,69 @@
 <!DOCTYPE html>
 <html>
-<body>
 <head>
     <meta charset="UTF-8">
     <meta name="keywords" content="HiOA Gaming membership registration page">
     <meta name="author" content="Andreas Jacobsen">
+    <meta name="author" content="Kristian Munter Simonsen">
 
 
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/jquery-ui.min.js"></script>
     <script src="birthdaypicker/bday-picker.min.js"></script>
-    <title>HiOA Gaming members </title>
-    <link rel="stylesheet" type="text/css" href="style.css"></head>
-
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>HiOA Gaming members </title></head>
+<body background="Pic/background.png">
+<div class="dropdown">
+    <button class="dropbtn">Member database</button>
+    <div class="dropdown-content">
+        <a href="#">Add users</a>
+        <a href="#">Administrate users</a>
+        <a href="#">Link 3</a>
+    </div>
+</div>
+<div class="table">
 <img src="Pic/logo.png" alt="Logo for HiOA Gaming" align="middle" height="200" width="400" >
 <h1>Registration for membership</h1>
 
 
     <form action="" method="get">
-    <br><b>First name:</b> <input type="text" name="FirstName" required></br>
-    <br><b>Last name:</b> <input type="text" name="LastName" required></br>
-    <br> <b>Stundet?</b> <br><select required name="student"></br>
+    <div>First name: <input type="text" name="FirstName" title="FirstName" required><br><br></div>
+    Last name: <input type="text" name="LastName" title="LastName" required><br>
+    <p> Stundet? <select required name="student" title="Student">
             <option value="1">Student</option>
-            <option value="0">Ikke student</option>
+            <option value="0">Not student</option>
         </select>
+        <p>Date of birth:</p>
         <!-- Birthday picker made by abecoffman
          github repo: https://github.com/abecoffman/birthdaypicker -->
-        <!-- TODO: Fikse stylen på denne shiten! -->
-        <br><script type="text/javascript">
+        <script type="text/javascript">
             $(document).ready(function () {
                 $("#picker2").birthdaypicker({
                     maxAge: 120,
                     dateFormat: "littleEndian",
-                    monthFormat: "long",
+                    monthFormat: "long"
                 });
             });
-        </script></br>
-    <br><b>Birht date:</b><div class="picker" id="picker2"></div>
+        </script>
+    <div class="picker" id="picker2"></div>
     <br>
-    <b>Payment:</b> <br>
-    <input type="radio" name="semester" value="Semester payment"> Semester member<br>
-    <input type="radio" name="semester" value="Yearly payment"> Yearly member<br>
-    <br><b>Gender:</b> <br>
-    <input type="radio" name="gender" value="Man"> Man<br>
-    <input type="radio" name="gender" value="Woman"> Woman<br>
+    <br>Payment: <br>
+    <input type="radio" name="semester" value="Semester payment" title="SemesterMember"> Semester member<br>
+    <input type="radio" name="semester" value="Yearly payment" title="YearlyMember"> Yearly member<br>
+    <br>Gender: <br>
+    <input type="radio" name="gender" value="Man" title="Man"> Man<br>
+    <input type="radio" name="gender" value="Woman" title="Woman"> Woman<br>
 
 
-    <br><input type="submit" value="Submit" id="submit" name="submit" class="submit">
+    <br><input type="submit" value="submit" id="submit" name="submit" title="submit">
 
 </form>
 
 
 <?php
-
+echo "PHP Kjører!" ; 
 if (isset($_GET['submit'])) {
+    echo "<br>lol!";
 
 
     $servername = "localhost";
@@ -78,6 +88,7 @@ if (isset($_GET['submit'])) {
         $gender_converter = "F";
     }
     $date = date('m/d/Y');
+
 
     $sql = "INSERT INTO members (first_name, last_name, student, gender, join_date)
         VALUES ('$first_name', '$last_name','$student', '$gender_converter','$date')";
